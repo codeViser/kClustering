422f5ec83f1cce3a66987fbbba550493cee8d478
codeViser <shubham.note3@gmail.com>
2016-06-18 17:13:22 +0530

    single iteration of k-means

---
+++ generate_samples.py
@@ -0,0 +1,69 @@
+# import tensorflow as tf
+# import numpy as np
+
+
+# from functions import create_samples
+# from functions import plot_clusters
+
+# n_features = 2
+# n_clusters = 3
+# n_samples_per_cluster = 500
+# seed = 700
+# embiggen_factor = 70
+
+# np.random.seed(seed)
+
+# centroids, samples = create_samples(n_clusters, n_samples_per_cluster, n_features, embiggen_factor, seed)
+
+# model = tf.initialize_all_variables()
+# with tf.Session() as session:
+#     sample_values = session.run(samples)
+#     centroid_values = session.run(centroids)
+#     plot_clusters(sample_values, centroid_values, n_samples_per_cluster)
+
+
+# import tensorflow as tf
+# import numpy as np
+
+# from functions import create_samples, choose_random_centroids, plot_clusters
+
+# n_features = 2
+# n_clusters = 3
+# n_samples_per_cluster = 500
+# seed = 700
+# embiggen_factor = 70
+
+# centroids, samples = create_samples(n_clusters, n_samples_per_cluster, n_features, embiggen_factor, seed)
+# initial_centroids = choose_random_centroids(samples, n_clusters)
+
+# model = tf.initialize_all_variables()
+# with tf.Session() as session:
+#     sample_values = session.run(samples)
+#     updated_centroid_value = session.run(initial_centroids)
+
+# plot_clusters(sample_values, updated_centroid_value, n_samples_per_cluster)
+
+import tensorflow as tf
+import numpy as np
+
+from functions import *
+
+n_features = 2
+n_clusters = 3
+n_samples_per_cluster = 500
+seed = 700
+embiggen_factor = 70
+
+
+data_centroids, samples = create_samples(n_clusters, n_samples_per_cluster, n_features, embiggen_factor, seed)
+initial_centroids = choose_random_centroids(samples, n_clusters)
+nearest_indices = assign_to_nearest(samples, initial_centroids)
+updated_centroids = update_centroids(samples, nearest_indices, n_clusters)
+
+model = tf.initialize_all_variables()
+with tf.Session() as session:
+    sample_values = session.run(samples)
+    updated_centroid_value = session.run(updated_centroids)
+    print(updated_centroid_value)
+
+plot_clusters(sample_values, updated_centroid_value, n_samples_per_cluster)
