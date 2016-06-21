5790e17452cf92534ee10b03e0736e4b863ccce2
codeViser <shubham.note3@gmail.com>
2016-06-21 18:10:54 +0530

    iteration complete max seed 4294967294

--- generate_samples.py
+++ generate_samples.py
@@ -53,14 +53,14 @@ from functions import *
 n_features = 2
 n_clusters = 3
 n_samples_per_cluster = 500
-seed = 700
+seed = 700 #seed_max=4294967295-1
 embiggen_factor = 70
+threshold = 0.005
 
 
 data_centroids, samples = create_samples(n_clusters, n_samples_per_cluster, n_features, embiggen_factor, seed)
 initial_centroids = choose_random_centroids(samples, n_clusters,seed)
 x=tf.Variable(initial_centroids,name="x")
-# x=tf.identity(initial_centroids)
 nearest_indices = assign_to_nearest(samples, x)
 updated_centroids = update_centroids(samples, nearest_indices, n_clusters)
 
@@ -70,22 +70,28 @@ with tf.Session() as session:
 	session.run(model)
 	sample_values = session.run(samples)
 	initial_centroids_values = session.run(initial_centroids)
-	# print(initial_centroids_values)
 	y=initial_centroids_values
 	assign_x=x.assign(y)
 	session.run(assign_x)
-	# print(x.eval())
-	#x=tf.identity(initial_centroids)
-	#updated_centroid_value = session.run(updated_centroids)
 
-	for i in range(100):
+	j = threshold*1.0001
+
+	while j > threshold:
 		updated_centroid_value=session.run(updated_centroids)
+
+		distances = tf.reduce_sum( tf.square(tf.sub(updated_centroids, x)), 1)
+		maxDist = tf.argmax(distances,0)
+		maxDist = tf.to_int32(maxDist)
+		distances_value=distances.eval()
+		j = distances_value[maxDist.eval()]
+
+		print(j)
+
 		z=updated_centroid_value
 		assign_x_new=x.assign(z)
 		session.run(assign_x_new)
 		print(x.eval())
-		plot_clusters(sample_values,updated_centroid_value,n_samples_per_cluster)
 
-	print(updated_centroid_value)
+	#print(updated_centroid_value)
 
-#plot_clusters(sample_values, updated_centroid_value, n_samples_per_cluster)
+plot_clusters(sample_values, updated_centroid_value, n_samples_per_cluster)
